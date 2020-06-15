499d9fd16422bd0729224416057964aaa27bed77
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-15 00:58:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,7 @@ class Deciduous extends Tree {
     super(species)
     this.name = name
   }
+  static definition(){
+    return `${super.definition()} Deciduous trees shed their leaves annually.`
+  }
 }
